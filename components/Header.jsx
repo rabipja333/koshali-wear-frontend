8014615e5c49680f14d04d9ca47ab6c46ee12f49
rabipja333 <131441424+rabipja333@users.noreply.github.com>
2023-04-22 19:12:29 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,118 +13,140 @@ import { fetchDataFromApi } from "@/utils/api";
 import { useSelector } from "react-redux";
 
 //koshli
-import { MdAccountCircle } from 'react-icons/Md';
+import { MdAccountCircle } from "react-icons/Md";
 //koshli
-const Header = () => {
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [showCatMenu, setShowCatMenu] = useState(false);
-    const [show, setShow] = useState("translate-y-0");
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [categories, setCategories] = useState(null);
+const Header = (logout, user) => {
+    const [dropdown, setDropdown] = useState(false)
+  const toggleDropdown= ()=>{
+    setDropdown(!dropdown)
+  }
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState("translate-y-0");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [categories, setCategories] = useState(null);
 
-    const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems } = useSelector((state) => state.cart);
 
-    const controlNavbar = () => {
-        if (window.scrollY > 200) {
-            if (window.scrollY > lastScrollY && !mobileMenu) {
-                setShow("-translate-y-[80px]");
-            } else {
-                setShow("shadow-sm");
-            }
-        } else {
-            setShow("translate-y-0");
-        }
-        setLastScrollY(window.scrollY);
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("-translate-y-[80px]");
+      } else {
+        setShow("shadow-sm");
+      }
+    } else {
+      setShow("translate-y-0");
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
     };
+  }, [lastScrollY]);
 
-    useEffect(() => {
-        window.addEventListener("scroll", controlNavbar);
-        return () => {
-            window.removeEventListener("scroll", controlNavbar);
-        };
-    }, [lastScrollY]);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
-    useEffect(() => {
-        fetchCategories();
-    }, []);
+  const fetchCategories = async () => {
+    const { data } = await fetchDataFromApi("/api/categories?populate=*");
+    setCategories(data);
+  };
 
-    const fetchCategories = async () => {
-        const { data } = await fetchDataFromApi("/api/categories?populate=*");
-        setCategories(data);
-    };
+  return (
+    <header
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+    >
 
-    return (
-        <header
-            className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
-        >
+         {/* DROPDOWN & LOGIN BUTTON FROM KW START*/}
+      <div className="cursor-pointer items-center cart absolute left-0 mx-5 top-4 flex">
+        <a onMouseOver={()=>{setDropdown(true)}} onMouseLeave={()=>{setDropdown(false)}}>
+        {user.value && <MdAccountCircle className='text-xl md:text-2xl mx-2'/>}
+      {dropdown && <div onMouseOver={()=>{setDropdown(true)}} onMouseLeave={()=>{setDropdown(false)}}className="absolute left-8 bg-white-lg border top-6 py-4 rounded-md px-5 w-32">
+        <ul>
+          <Link href={'/myaccount'}><li className='py-1 hover:text-pink-700 font-bold text-sm'>My Account</li></Link>
+          
+          <Link href={'/orders'}><li className='py-1 hover:text-pink-700 font-bold text-sm'>Order</li></Link>
+          
+          <li onClick={logout} className='py-1 hover:text-pink-700 font-bold text-sm'>Log out</li>
+        </ul>
+      </div>}
 
+      
+      </a>
+        {!user.value && <Link href={'/login'}>
+          <button className='bg-pink-600 px-2 py-1 rounded-md text-sm text-white mx-2'>Login</button>
+        </Link>}
+      </div>
 
-<Link href={'/login'}><div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-<MdAccountCircle className="text-[20px] md:text-[25px]" /></div></Link>
+       {/* DROPDOWN & LOGIN BUTTON FROM KW STOP*/}
 
+      <Wrapper className="h-[60px] flex justify-between items-center">
+        <Link href="/">
+          <img src="/logo.svg" className=" left-32 w-[40px] md:w-[60px]" />
+        </Link>
 
-            <Wrapper className="h-[60px] flex justify-between items-center">
-                <Link href="/">
-                    <img src="/logo.svg" className="w-[40px] md:w-[60px]" />
-                </Link>
+        <Menu
+          showCatMenu={showCatMenu}
+          setShowCatMenu={setShowCatMenu}
+          categories={categories}
+        />
 
-                <Menu
-                    showCatMenu={showCatMenu}
-                    setShowCatMenu={setShowCatMenu}
-                    categories={categories}
-                />
+        {mobileMenu && (
+          <MenuMobile
+            showCatMenu={showCatMenu}
+            setShowCatMenu={setShowCatMenu}
+            setMobileMenu={setMobileMenu}
+            categories={categories}
+          />
+        )}
 
-                {mobileMenu && (
-                    <MenuMobile
-                        showCatMenu={showCatMenu}
-                        setShowCatMenu={setShowCatMenu}
-                        setMobileMenu={setMobileMenu}
-                        categories={categories}
-                    />
-                )}
+        <div className="flex items-center gap-2 text-black">
+          {/* Icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              51
+            </div>
+          </div>
+          {/* Icon end */}
 
-                <div className="flex items-center gap-2 text-black">
-                    {/* Icon start */}
-                    <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-                        <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
-                        <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
-                            51
-                        </div>
-                    </div>
-                    {/* Icon end */}
-
-                    {/* Icon start */}
-                    <Link href="/cart">
-                        <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-                            <BsCart className="text-[15px] md:text-[20px]" />
-                            {cartItems.length > 0 && (
-                                <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
-                                    {cartItems.length}
-                                </div>
-                            )}
-                        </div>
-                    </Link>
-                    {/* Icon end */}
-
-                    {/* Mobile icon start */}
-                    <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
-                        {mobileMenu ? (
-                            <VscChromeClose
-                                className="text-[16px]"
-                                onClick={() => setMobileMenu(false)}
-                            />
-                        ) : (
-                            <BiMenuAltRight
-                                className="text-[20px]"
-                                onClick={() => setMobileMenu(true)}
-                            />
-                        )}
-                    </div>
-                    {/* Mobile icon end */}
+          {/* Icon start */}
+          <Link href="/cart">
+            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+              <BsCart className="text-[15px] md:text-[20px]" />
+              {cartItems.length > 0 && (
+                <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+                  {cartItems.length}
                 </div>
-            </Wrapper>
-        </header>
-    );
+              )}
+            </div>
+          </Link>
+          {/* Icon end */}
+
+          {/* Mobile icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[16px]"
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[20px]"
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
+          </div>
+          {/* Mobile icon end */}
+        </div>
+      </Wrapper>
+    </header>
+  );
 };
 
 export default Header;
