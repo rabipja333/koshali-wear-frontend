210903ad0eb5b98d3cb7d05a3ac8cc178543d2c3
rabipja333 <131441424+rabipja333@users.noreply.github.com>
2023-04-22 15:12:29 +0530

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
+import { useRouter } from 'next/router';
+
+
 
 const Signup = () => {
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
+    const router=useRouter()
+    useEffect(() => {
+        if(localStorage.getItem('token')){
+          router.push('/')
+        }
+      }, [])
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     const handleChange = (e) => {
         if (e.target.name == 'name') {
@@ -22,7 +31,7 @@ const Signup = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         const data = { name, email, password }
-        let res = await fetch("http://localhost:3000/api/signup", {
+        let res = await fetch(`${process.env.HOST}/api/signup`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -59,30 +68,30 @@ const Signup = () => {
                 pauseOnHover
                 theme="light"
             />
-            <body class="body bg-white dark:bg-[#280f2a]">
-                <div class="bg-black before:animate-pulse before:bg-gradient-to-b before:from-gray-900 overflow-hidden before:via-[#00FF00] before:to-gray-900 before:absolute ">
+           
+                <div className="bg-black before:animate-pulse before:bg-gradient-to-b before:from-gray-900 overflow-hidden before:via-[#00FF00] before:to-gray-900 before:absolute ">
                     <div id="myDIV" >
-                        <div class="w-[100vw] h-[100vh] px-3 sm:px-5 flex items-center justify-center absolute">
-                            <div class="w-full sm:w-1/2 lg:2/3 px-6 bg-gray-500 bg-opacity-20 bg-clip-padding backdrop-filter backdrop-blur-sm text-white z-50 py-4  rounded-lg">
-                                <div class="w-full flex justify-center text-[#ff00ee] text-xl mb:2 md:mb-5">
+                        <div className="w-[100vw] h-[100vh] px-3 sm:px-5 flex items-center justify-center absolute">
+                            <div className="w-full sm:w-1/2 lg:2/3 px-6 bg-gray-500 bg-opacity-20 bg-clip-padding backdrop-filter backdrop-blur-sm text-white z-50 py-4  rounded-lg">
+                                <div className="w-full flex justify-center text-[#ff00ee] text-xl mb:2 md:mb-5">
                                     Creat an Account
                                 </div>
                                 <form onSubmit={handleSubmit} className='mt-8 space-y-6' method="POST">
                                     <input type="hidden" name="remember" value="true" />
-                                    <div class="mb-6">
-                                        <label for="name" class="block mb-2 text-xs font-medium text-black">Name</label>
-                                        <input value={name} onChange={handleChange} id="name" name="name" type="name" autoComplete="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Name" required />
+                                    <div className="mb-6">
+                                        <label htmlFor="name" className="block mb-2 text-xs font-medium text-black">Name</label>
+                                        <input value={name} onChange={e => setName(e.target.value)} id="name" name="name" type="name" autoComplete="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Name" required />
                                     </div>
-                                    <div class="mb-6">
-                                        <label for="email" class="block mb-2 text-xs font-medium text-black">Your email</label>
-                                        <input value={email} onChange={handleChange} id="email" name="email" type="email" autoComplete="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Email" required />
+                                    <div className="mb-6">
+                                        <label htmlFor="email" className="block mb-2 text-xs font-medium text-black">Your email</label>
+                                        <input  value={email} onChange={e => setEmail(e.target.value)} id="email" name="email" type="email" autoComplete="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Email" required />
                                     </div>
-                                    <div class="mb-6">
-                                        <label for="password" class="block mb-2 text-xs font-medium text-black">Your password</label>
-                                        <input value={password} onChange={handleChange} id="password" name="password" type="password" autoComplete="current-password" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Set a password" required />
+                                    <div className="mb-6">
+                                        <label htmlFor="password" className="block mb-2 text-xs font-medium text-black">Your password</label>
+                                        <input  value={password} onChange={e => setPassword(e.target.value)} id="password" name="password" type="password" autoComplete="current-password" className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Set a password" required />
                                     </div>
-                                    <div class="flex flex-row justify-between">
-                                        <Link href={'/login'} ><div class="text-[#ff00d9] text-sm md:text-md">Having ID?Login</div></Link>
+                                    <div className="flex flex-row justify-between">
+                                        <Link href={'/login'} ><div className="text-[#ff00d9] text-sm md:text-md">Having ID?Login</div></Link>
                                     </div>
                                     <div>
                                         <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset focus:ring-pink-500">
@@ -90,22 +99,24 @@ const Signup = () => {
                                                 <svg className='h-5 text-pink-500 group-hover:text-pink-400' xmlns="http://www.w3.org/2000/svg" viewBox='0 0 20 20' fill='currentColour' aria-hidden="true">
                                                     <path fillRule='evenodd' d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                                                 </svg>
-                                            </span>Sign Up</button>
+                                            </span>
+                                            Sign Up
+                                            </button>
                                     </div>
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
-            </body>
+            
 
 
             {/* <script>
                 const para = document.createElement("div");
                 para.className = 'flex flex-wrap gap-0.5 h-screen items-center justify-center  relative';
-                let el = '<div class="  transition-colors duration-[1.5s] hover:duration-[0s] border-[#00FF00] h-[calc(5vw-2px)] w-[calc(5vw-2px)] md:h-[calc(4vw-2px)] md:w-[calc(4vw-2px)] lg:h-[calc(3vw-4px)] lg:w-[calc(3vw-4px)] bg-gray-900 hover:bg-[#00FF00]"></div>'
+                let el = '<div className="  transition-colors duration-[1.5s] hover:duration-[0s] border-[#00FF00] h-[calc(5vw-2px)] w-[calc(5vw-2px)] md:h-[calc(4vw-2px)] md:w-[calc(4vw-2px)] lg:h-[calc(3vw-4px)] lg:w-[calc(3vw-4px)] bg-gray-900 hover:bg-[#00FF00]"></div>'
                 for (let k = 1; k=1000; k++)
-                {el += '<div class = " transition-colors duration-[1.5s] hover:duration-[0s] border-[#00FF00] h-[calc(5vw-2px)] w-[calc(5vw-2px)] md:h-[calc(4vw-2px)] md:w-[calc(4vw-2px)] lg:h-[calc(3vw-4px)] lg:w-[calc(3vw-4px)] bg-gray-900 hover:bg-[#00FF00]"></div>'}
+                {el += '<div className = " transition-colors duration-[1.5s] hover:duration-[0s] border-[#00FF00] h-[calc(5vw-2px)] w-[calc(5vw-2px)] md:h-[calc(4vw-2px)] md:w-[calc(4vw-2px)] lg:h-[calc(3vw-4px)] lg:w-[calc(3vw-4px)] bg-gray-900 hover:bg-[#00FF00]"></div>'}
 
 
                 para.innerHTML = el;
@@ -115,11 +126,50 @@ const Signup = () => {
     )
 }
 
-export default Signup
-//class="mt-4 md:mt-10 w-full flex justify-center text-sm md:text-xl bg-[#ff00f2] py-2 rounded-md"
+
+//className="mt-4 md:mt-10 w-full flex justify-center text-sm md:text-xl bg-[#ff00f2] py-2 rounded-md"
 // type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset focus:ring-pink-500">
 //                                             <span className='absolute left-0 insert-y-0 flex items-center pl-3'>
 //                                                  <svg className='h-5 text-pink-500 group-hover:text-pink-400' xmlns="http://www.w3.org/2000/svg" viewBox='0 0 20 20' fill='currentColour' aria-hidden="true">
 //                                                      <path fillRule='evenodd' d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
 //                                                  </svg>
 //                                              </span>
+
+
+
+
+{/* <div className="w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+  <div className="w-full sm:max-w-md p-5 mx-auto">
+    <h2 className="mb-12 text-center text-5xl font-extrabold">Creat an Account</h2>
+    <form onSubmit={handleSubmit}>
+    <div className="mb-4">
+        <label className="block mb-1" for="name">Name</label>
+        <input value={name} onChange={handleChange} id="name" type="neme" name="name" className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+      </div>
+      <div className="mb-4">
+        <label className="block mb-1" for="email">Email-Address</label>
+        <input value={email} onChange={handleChange} id="email" type="text" name="email" className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+      </div>
+      <div className="mb-4">
+        <label className="block mb-1" for="password">Password</label>
+        <input value={password} onChange={handleChange} id="password" type="password" name="password" className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+      </div>
+      
+      <div className="mt-6 flex items-center justify-between">
+        <div className="flex items-center">
+          <input id="remember_me" type="checkbox" className="border border-gray-300 text-red-600 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50" />
+          <label for="remember_me" className="ml-2 block text-sm leading-5 text-gray-900"> Remember me </label>
+        </div>
+        <a href="#" className="text-sm"> Forgot your password? </a>
+      </div>
+      <div className="mt-6">
+        <button className="w-full inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition">Sign In</button>
+      </div>
+      <div className="mt-6 text-center">
+        <a href="#" className="underline">Sign up for an account</a>
+      </div>
+    </form>
+  </div>
+</div>*/}
+
+export default Signup; 
